3720d38939755b355f62e73583846fd763480e72
Barbara0Prado <62604479+Barbara0Prado@users.noreply.github.com>
2022-05-03 16:24:46 +0100

    connecting to mongodb atlas

--- index.js
+++ index.js
@@ -1,2 +1,37 @@
 //Initial configuration
-const express = require('express')
+const express = require ('express')
+const mongoose = require ('mongoose')
+const app = express ()
+
+//Reading JSON // middlewares
+app.use(
+  express.urlencoded ({
+    extended: true
+  })
+)
+
+app.use(express.json ())
+
+// Endpoint
+app.get ('/',  (req, res) => {
+
+//Show req
+
+res.json ({message: 'Hi Express!' })
+})
+
+// Port to be accessed
+const DB_USER ='barbara'
+const DB_PASSWORD = encodeURIComponent ('dogsapi123')
+
+mongoose.connect (
+  `mongodb+srv://${DB_USER}:${DB_PASSWORD}@apicluster.mjkcp.mongodb.net/databaseApi?retryWrites=true&w=majority`,
+  )
+  .then(() => {
+    console.log ('We are connected to MongoDB!')
+    app.listen (3000)
+
+  })
+  .catch((err) => console.log(err))
+
+app.listen(3000)
